870fdac0fa8c4c3c01c3334506071ff7c86132fa
Aaron <abarnard@protonmail.com>
2020-04-20 10:37:02 +1000

    Edit yaml

--- .circleci/trigger-demo-build.sh
+++ .circleci/trigger-demo-build.sh
@@ -1,3 +1,1 @@
-#!/bin/bash
-TOKEN=$1
-curl -X POST --header "Content-Type: application/json" -d '{"build_parameters": {"CIRCLE_JOB": "deploy_stage"}}' https://circleci.com/api/v1/project/blocknative/react-demo/tree/develop?circle-token=$TOKEN
+curl -X POST --header "Content-Type: application/json" -d '{"build_parameters": {"CIRCLE_JOB": "deploy_stage"}}' https://circleci.com/api/v1/project/blocknative/react-demo/tree/develop?circle-token=$1
