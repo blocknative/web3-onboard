9ed97f9f4f00de9a9c8fe13570c7b96c5b6e4cb0
Aaron <abarnard@protonmail.com>
2020-06-22 08:35:02 +1000

    Consistent code style for subscriptions

--- src/onboard.ts
+++ src/onboard.ts
@@ -130,9 +130,9 @@ function init(initialization: Initialization): API {
 
     if (subscriptions.wallet) {
       wallet.subscribe((wallet: Wallet) => {
-        wallet.provider !== null &&
-          subscriptions.wallet &&
-          subscriptions.wallet(wallet)
+        if (wallet.provider !== null) {
+          subscriptions.wallet && subscriptions.wallet(wallet)
+        }
       })
     }
   }
