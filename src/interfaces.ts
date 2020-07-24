c731d99e1f29a372095e52265355e64a39748770
chaitanyapotti <chaitanya.potti@gmail.com>
2020-07-24 19:38:15 +0800

    Update torus

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -192,22 +192,119 @@ export interface LedgerOptions extends CommonWalletOptions {
   LedgerTransport?: any
 }
 
-export interface TorusOptions extends CommonWalletOptions {
-  loginMethod?: 'google' | 'facebook' | 'twitch' | 'reddit' | 'discord'
-  buildEnv?: 'production' | 'development' | 'staging' | 'testing'
-  showTorusButton?: boolean
-  buttonPosition?: 'top-left' | 'top-right' | 'bottom-right' | 'bottom-left'
-  enableLogging?: boolean
-  enabledVerifiers?: TorusVerifierStatus
-}
+//#region torus
 
-interface TorusVerifierStatus {
+interface VerifierStatus {
   google?: boolean
   facebook?: boolean
   reddit?: boolean
   twitch?: boolean
   discord?: boolean
 }
+
+type LOGIN_TYPE =
+  | 'google'
+  | 'facebook'
+  | 'reddit'
+  | 'discord'
+  | 'twitch'
+  | 'apple'
+  | 'github'
+  | 'linkedin'
+  | 'twitter'
+  | 'weibo'
+  | 'line'
+  | 'jwt'
+  | 'email-password'
+  | 'passwordless'
+
+interface BaseLoginOptions {
+  display?: 'page' | 'popup' | 'touch' | 'wap'
+  prompt?: 'none' | 'login' | 'consent' | 'select_account'
+  max_age?: string | number
+  ui_locales?: string
+  id_token_hint?: string
+  login_hint?: string
+  acr_values?: string
+  scope?: string
+  audience?: string
+  connection?: string
+  [key: string]: unknown
+}
+
+interface JwtParameters extends BaseLoginOptions {
+  domain: string
+  client_id?: string
+  redirect_uri?: string
+  leeway?: number
+  verifierIdField?: string
+  isVerifierIdCaseSensitive?: boolean
+}
+
+interface IntegrityParams {
+  check: boolean
+  hash?: string
+  version?: string
+}
+
+interface WhiteLabelParams {
+  theme: ThemeParams
+  defaultLanguage?: string
+  logoDark: string
+  logoLight: string
+  topupHide?: boolean
+  featuredBillboardHide?: boolean
+  disclaimerHide?: boolean
+  tncLink?: LocaleLinks<string>
+  privacyPolicy?: LocaleLinks<string>
+  contactLink?: LocaleLinks<string>
+  customTranslations?: LocaleLinks<any>
+}
+
+interface LocaleLinks<T> {
+  en?: T
+  ja?: T
+  ko?: T
+  de?: T
+  zh?: T
+}
+
+interface ThemeParams {
+  isDark: boolean
+  colors: any
+}
+
+interface LoginConfigItem {
+  name?: string
+  typeOfLogin: LOGIN_TYPE
+  description?: string
+  clientId?: string
+  logoHover?: string
+  logoLight?: string
+  logoDark?: string
+  showOnModal?: boolean
+  jwtParameters?: JwtParameters
+}
+
+interface LoginConfig {
+  [verifier: string]: LoginConfigItem
+}
+
+export interface TorusOptions extends CommonWalletOptions {
+  buttonPosition?: 'top-left' | 'top-right' | 'bottom-right' | 'bottom-left'
+  modalZIndex?: number
+  apiKey?: string
+  buildEnv?: 'production' | 'development' | 'staging' | 'testing' | 'lrc'
+  enableLogging?: boolean
+  enabledVerifiers?: VerifierStatus
+  loginConfig?: LoginConfig
+  showTorusButton?: boolean
+  integrity?: IntegrityParams
+  whiteLabel?: WhiteLabelParams
+  loginMethod?: 'google' | 'facebook' | 'twitch' | 'reddit' | 'discord' | string
+}
+
+//#endregion torus
 
 export interface AuthereumOptions extends CommonWalletOptions {
   apiKey?: string
