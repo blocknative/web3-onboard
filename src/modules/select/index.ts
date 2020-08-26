34b3b6d8e79e6bc910be01979583302ed4b4c1ab
MacOMNI <414294494@qq.com>
2020-08-26 11:02:06 +0800

    # add hyperpay wallet

--- src/modules/select/index.ts
+++ src/modules/select/index.ts
@@ -96,6 +96,8 @@ function getModule(name: string): Promise<any> | undefined {
       return import('./wallets/mykey')
     case 'huobiwallet':
       return import('./wallets/huobiwallet')
+    case 'hyperpay':
+      return import('./wallets/hyperpay')
     default:
       return
   }
