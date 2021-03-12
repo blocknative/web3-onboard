a0e65027a9a90f1c324b25bef7e82a091bfbb400
Tom Tan <jtomtan@qq.com>
2021-03-12 12:06:47 +0800

    Add Ownbit
    Add Ownbit

--- src/modules/select/wallets/ownbit.ts
+++ src/modules/select/wallets/ownbit.ts
@@ -7,65 +7,30 @@ import {
 
 import ownbitIcon from '../wallet-icons/icon-ownbit'
 
-function ownbit(options: InjectedWithBalanceOptions): WalletModule {
-  const { preferred, label, svg, rpcUrl } = options
+function ownbit(options: CommonWalletOptions): WalletModule {
+  const { preferred, label, iconSrc, svg } = options
 
   return {
     name: label || 'Ownbit',
+    iconSrc,
     svg: svg || ownbitIcon,
     wallet: async (helpers: Helpers) => {
-      const { getProviderName, getAddress, getNetwork, getBalance } = helpers
-      const ownbitProvider =
-        (window as any).ethereum ||
-        ((window as any).web3 && (window as any).web3.currentProvider)
+            const { getProviderName, createModernProviderInterface } = helpers
 
-      const isOwnbit =
-        getProviderName(ownbitProvider) === 'Ownbit'
-      let createProvider
+            const provider =
+                (window as any).ethereum ||
+                ((window as any).web3 && (window as any).web3.currentProvider)
 
-      if (isOwnbit && rpcUrl) {
-        createProvider = (await import('./providerEngine')).default
-      }
-
-      const provider = createProvider ? createProvider({ rpcUrl }) : null
-
-      let warned = false
-
-      return {
-        provider: ownbitProvider,
-        interface: isOwnbit
-          ? {
-              address: {
-                get: () => getAddress(ownbitProvider)
-              },
-              network: {
-                get: () => getNetwork(ownbitProvider)
-              },
-              balance: {
-                get: async () => {
-                  if (!provider) {
-                    if (!warned) {
-                      console.warn(
-                        'The Ownbit Wallet provider does not allow rpc calls preventing Onboard.js from getting the balance. You can pass in a "rpcUrl" to the Ownbit Wallet initialization object to get the balance.'
-                      )
-                      warned = true
-                    }
-
-                    return null
-                  }
-
-                  const address = await getAddress(ownbitProvider)
-
-                  return getBalance(provider, address)
-                }
-              },
-              name: getProviderName(ownbitProvider)
+            return {
+                provider,
+                interface:
+                    (getProviderName(provider) === 'Ownbit' &&
+                        createModernProviderInterface(provider)) ||
+                    null
             }
-          : null
-      }
     },
     type: 'injected',
-    link: 'ownbit://navigate?screen=DappView&url=${window.location.href}',
+    link: 'https://ownbit.io',
     installMessage: mobileWalletInstallMessage,
     mobile: true,
     preferred
