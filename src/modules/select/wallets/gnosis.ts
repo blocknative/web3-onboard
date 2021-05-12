cde665e4f2adea9d00fa3c73b31c80f100a32580
Taylor Dawson <taylorjdawson@gmail.com>
2021-05-11 20:58:31 -0700

    Convert the wallet implementation to a function

--- src/modules/select/wallets/gnosis.ts
+++ src/modules/select/wallets/gnosis.ts
@@ -1,64 +1,54 @@
 import type SafeAppsSDK from '@gnosis.pm/safe-apps-sdk'
 import type { SafeInfo } from '@gnosis.pm/safe-apps-sdk'
-import {
-  CommonWalletOptions,
-  Helpers,
-  WalletModule,
-  WalletType
-} from '../../../interfaces'
+import { CommonWalletOptions, Helpers, WalletModule } from '../../../interfaces'
 import gnosisWalletIcon from '../wallet-icons/icon-gnosis'
 
-interface InstallMessageWallets {
-  currentWallet: string | undefined
-  selectedWallet: string
+const getSafe = (sdk: SafeAppsSDK): Promise<SafeInfo | undefined> => {
+  return Promise.race([
+    sdk.getSafeInfo(),
+    // Timeout need as this method hangs until it can find the safe info
+    new Promise<undefined>(resolve => setTimeout(resolve, 200))
+  ])
 }
 
-class GnosisSafeWallet implements WalletModule {
-  name = 'Gnosis Safe Wallet'
-  type: WalletType = 'sdk'
-  svg: string = gnosisWalletIcon
-  mobile = false
-  desktop = true
-  link: string
+function gnosis(options: CommonWalletOptions): WalletModule {
+  const { preferred, label, iconSrc, svg, networkId } = options
 
-  constructor(options: CommonWalletOptions) {
-    // Override class instance methods with wallet options when provided i.e. name, svg, .ect
-    Object.assign(this, {
-      ...(options.label ? { name: options.label } : {}),
-      ...options
-    })
-    const network = options.networkId === 4 ? 'rinkeby.' : ''
-    this.link = `https://${network}gnosis-safe.io/app`
-  }
+  const network = networkId === 4 ? 'rinkeby.' : ''
+  const link = `https://${network}gnosis-safe.io/app`
 
-  async wallet({ createModernProviderInterface }: Helpers) {
-    const sdk = new (await import('@gnosis.pm/safe-apps-sdk')).default()
-    const { SafeAppProvider } = await import('@gnosis.pm/safe-apps-provider')
+  return {
+    name: label || 'Gnosis Safe Wallet',
+    iconSrc,
+    svg: svg || gnosisWalletIcon,
+    wallet: async ({ createModernProviderInterface }: Helpers) => {
+      const sdk = new (await import('@gnosis.pm/safe-apps-sdk')).default()
+      const { SafeAppProvider } = await import('@gnosis.pm/safe-apps-provider')
 
-    const safe: SafeInfo | undefined = await GnosisSafeWallet.getSafe(sdk)
-    if (!safe) return { provider: undefined, interface: null }
-    const provider = new SafeAppProvider(safe, sdk)
-    return {
-      provider,
-      interface: {
-        ...createModernProviderInterface(provider),
-        connect: () => Promise.resolve([safe.safeAddress])
+      const safe: SafeInfo | undefined = await getSafe(sdk)
+      // Checks if we are within the safe app context
+      if (!safe) return { provider: undefined, interface: null }
+
+      const provider = new SafeAppProvider(safe, sdk)
+      return {
+        provider,
+        interface: {
+          ...createModernProviderInterface(provider),
+          connect: () => Promise.resolve([safe.safeAddress])
+        }
       }
-    }
-  }
-
-  /**
-   * Tries to retrieve safe info and returns undefined if this isn't called
-   * within the Gnosis safe interface.
-   */
-  static getSafe(sdk: SafeAppsSDK): Promise<SafeInfo | undefined> {
-    return Promise.race([
-      sdk.getSafeInfo(),
-      // Timeout need as this method hangs until it can find the safe info
-      new Promise<undefined>(resolve => setTimeout(resolve, 200))
-    ])
+    },
+    type: 'sdk',
+    link,
+    installMessage: () => `
+        <p style="font-size: 0.889rem; font-family: inherit; margin: 0.889rem 0;">
+            Click the button below to open the Gnosis Safe interface.
+        </p>
+        `,
+    desktop: true,
+    mobile: false,
+    preferred
   }
 }
 
-export default (options: CommonWalletOptions): WalletModule =>
-  new GnosisSafeWallet(options)
+export default gnosis
