cec71cccb488a3cede198d4e1770b07dddfd5eab
roy1210 <rswkure@gmail.com>
2021-06-27 02:51:28 +0800

    fix: run lint

--- src/modules/select/wallets/binance-chain-wallet.ts
+++ src/modules/select/wallets/binance-chain-wallet.ts
@@ -2,27 +2,26 @@ import { CommonWalletOptions, Helpers, WalletModule } from '../../../interfaces'
 import { extensionInstallMessage, mobileWalletInstallMessage } from '../content'
 import { binanceChainWalletLogo } from '../wallet-icons/icon-binance-chain-wallet'
 
-
 function binanceChainWallet(
   options: CommonWalletOptions & { isMobile: boolean }
 ): WalletModule {
   const { preferred, label, iconSrc, svg, isMobile } = options
 
   return {
-    name: label  || 'Binance Chain Wallet',
+    name: label || 'Binance Chain Wallet',
     iconSrc: iconSrc || binanceChainWalletLogo,
     iconSrcSet: iconSrc || binanceChainWalletLogo,
     svg: svg || binanceChainWalletLogo,
     wallet: async (helpers: Helpers) => {
-      const { createModernProviderInterface } = helpers;
+      const { createModernProviderInterface } = helpers
 
       // Ref: https://binance-wallet.gitbook.io/binance-chain-extension-wallet
-      const provider = (window as any).BinanceChain;
+      const provider = (window as any).BinanceChain
 
       return {
         provider,
-        interface: provider && createModernProviderInterface(provider),
-      };
+        interface: provider && createModernProviderInterface(provider)
+      }
     },
     type: 'injected',
     link: 'https://docs.binance.org/smart-chain/wallet/binance.html#download-link',
