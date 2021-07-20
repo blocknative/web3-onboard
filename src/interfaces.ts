aa5e5bca3af57a94923e63b451249e898069c423
Taylor Dawson <taylorjdawson@gmail.com>
2021-07-19 22:54:13 -0700

    Adds ens support

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -13,6 +13,7 @@ export interface Initialization {
 
 export interface Subscriptions {
   address?: (address: string) => void
+  ens?: (ens: Ens) => void
   network?: (networkId: number) => void
   balance?: (balance: string) => void
   wallet?: (wallet: Wallet) => void
@@ -551,4 +552,10 @@ export interface TermsAgreementState {
   version: string
   terms?: boolean
   privacy?: boolean
+}
+
+export interface Ens {
+  name?: string
+  contentHash?: string
+  getText?: (key: string) => Promise<string | undefined>
 }
