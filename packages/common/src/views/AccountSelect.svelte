dc24fd265a46ad74cc6c3940116d1aaab6464a75
Adam Carpenter <Adam@blocknative.com>
2021-12-23 09:38:43 -0700

    Add border radius to background of input toggle to make extra clean

--- packages/common/src/views/AccountSelect.svelte
+++ packages/common/src/views/AccountSelect.svelte
@@ -115,7 +115,7 @@
     display: none;
   }
 
-  input {
+  input[type='text'] {
     display: block;
     margin: 0;
     -moz-appearance: none;
@@ -123,7 +123,7 @@
     appearance: none;
     scrollbar-width: none;
     width: 100%;
-    padding: 0.5rem 1rem;
+    padding: 0.5rem 2.6rem 0.5rem 1rem;
     border-radius: 8px;
     font-size: var(--account-select-font-size-5, var(--font-size-5));
     line-height: var(--account-select-font-line-height-1, var(--font-line-height-1));
@@ -267,7 +267,7 @@
     margin-right: var(--account-select-margin-5, var(--margin-5));
   }
 
-  .input-select{
+  .input-select {
     background-image: url(data:image/svg+xml;charset=US-ASCII,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22292.4%22%20height%3D%22292.4%22%3E%3Cpath%20fill%3D%22%23242835%22%20d%3D%22M287%2069.4a17.6%2017.6%200%200%200-13-5.4H18.4c-5%200-9.3%201.8-12.9%205.4A17.6%2017.6%200%200%200%200%2082.2c0%205%201.8%209.3%205.4%2012.9l128%20127.9c3.6%203.6%207.8%205.4%2012.8%205.4s9.2-1.8%2012.8-5.4L287%2095c3.5-3.5%205.4-7.8%205.4-12.8%200-5-1.9-9.2-5.5-12.8z%22%2F%3E%3C%2Fsvg%3E), linear-gradient(to bottom, transparent 0%, transparent 100%);
     background-repeat: no-repeat, repeat;
     background-position: center;
@@ -278,6 +278,7 @@
     width: 2.5rem;
     height: 2.5rem;
     background-color: var(--account-select-white, var(--white));
+    border-radius: 1rem;
   }
   
   .asset-container {
@@ -316,7 +317,11 @@
             Select Base Path
           </h4>
           {#if customDerivationPath}
-            <input class='base-path-select' placeholder="type/your/custom/path..." on:change={e => handleCustomPath(e)}/> 
+            <input type='text' 
+              class='base-path-select' 
+              placeholder="type/your/custom/path..." 
+              on:change={e => handleCustomPath(e)}
+            /> 
             <span class='input-select' on:click={toggleDerivationPathToDropdown}></span>
           {:else if  !customDerivationPath}
             <select
