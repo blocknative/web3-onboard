28f837cc1081d53316ec6842e4964b8916c30cef
Adam Carpenter <Adam@blocknative.com>
2021-12-23 11:19:09 -0700

    Add sample typed data

--- packages/demo/src/App.svelte
+++ packages/demo/src/App.svelte
@@ -16,6 +16,10 @@
     new VConsole()
   }
 
+  let signTransactionMsg;
+  let signMsg;
+  let signTypedMsg;
+
   const injected = injectedModule({
     wallets: [
       // include custom injected wallet modules here
@@ -94,16 +98,30 @@
   // Subscribe to wallet updates
   const wallets$ = onboard.state.select('wallets').pipe(share())
 
-  const data = JSON.stringify({
-    types: {
-        EIP712Domain: domain,
-        Bid: bid,
-        Identity: identity,
+  const msgParams = {types:{
+      EIP712Domain:[
+        {name:"name",type:"string"},
+        {name:"version",type:"string"},
+        {name:"chainId",type:"uint256"},
+        {name:"verifyingContract",type:"address"}
+      ],
+      Person:[
+        {name:"name",type:"string"},
+        {name:"wallet",type:"address"}
+      ],
+      Mail:[
+        {name:"from",type:"Person"},
+        {name:"to",type:"Person"},
+        {name:"contents",type:"string"}
+      ]
     },
-    domain: domainData,
-    primaryType: "Bid",
-    message: ''
-  });
+    primaryType:"Mail",
+    domain:{name:"Ether Mail",version:"1",chainId:"0x4",verifyingContract:"0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC"},
+    message:{
+      from:{name:"Cow",wallet:"0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826"},
+      to:{name:"Bob",wallet:"0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB"},
+      contents:"Hello, Bob!"}
+    }
 </script>
 
 <style>
@@ -211,7 +229,7 @@
             <button on:click={() => {
               provider.request({
                 method: 'eth_signTypedData',
-                params: [address, signTypedMsg]
+                params: [address, JSON.stringify(msgParams)]
               })
             }}>
               Sign Typed Message
