3f0c6574e784627c5443a31713a34db36b2b1fb6
Adam Carpenter <adamcarpenter86@gmail.com>
2022-01-04 16:26:26 -0700

    Add transition to modal, give more space to the network dropdown (#770)

--- packages/common/src/views/AccountSelect.svelte
+++ packages/common/src/views/AccountSelect.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
+  import { fade } from 'svelte/transition'
   import blocknative from '../icons/blocknative'
   import CloseButton from '../elements/CloseButton.svelte'
   import AddressTable from '../elements/AddressTable.svelte'
-  import TableHeader from '../elements/TableHeader.svelte';
+  import TableHeader from '../elements/TableHeader.svelte'
   import { displayModal$ } from '../streams'
 
   
   import type { Subject } from 'rxjs'
-  import type { ScanAccountsOptions, SelectAccountOptions, Account, AccountsList } from '../types';
+  import type { ScanAccountsOptions, SelectAccountOptions, Account, AccountsList } from '../types'
 
   export let selectAccountOptions: SelectAccountOptions
   export let accounts$: Subject<Account[]>
@@ -95,7 +96,7 @@
     background-size: 0.65em auto, 100%;
     scrollbar-width: none;
     width: 100%;
-    padding: 0.5rem 1rem;
+    padding: 0.5rem 1.8rem 0.5rem 1rem;
     border-radius: 8px;
     font-size: var(--account-select-font-size-5, var(--font-size-5));
     line-height: var(--account-select-font-line-height-1, var(--font-line-height-1));
@@ -247,19 +248,19 @@
   }
 
   .base-path-select {
-    min-width: 25rem;
+    min-width: 20rem;
   }
 
   .asset-select {
-    width: 6rem
+    width: 6rem;
   }
   
   .network-select {
-    width: 8rem
+    min-width: 12rem;
   }
 
   .w-100 {
-    width: 100%
+    width: 100%;
   }
 
   .base-path-container {
@@ -305,7 +306,7 @@
 </style>
 
 {#if $displayModal$}
-  <div class='hardware-connect-modal'>
+  <div class='hardware-connect-modal' transition:fade>
     <header class='connect-wallet-header'>
       <div class='bn-logo'>{@html blocknative}</div>
       <div class='wallet-icon'>{@html walletIcon}</div>
