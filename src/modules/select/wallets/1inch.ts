c652d163b5e83fa76a2cf578022c646bcdfb8c4a
shoom3301 <shoom3301@gmail.com>
2022-01-14 18:44:25 +0400

    1inch wallet

---
+++ src/modules/select/wallets/1inch.ts
@@ -0,0 +1,41 @@
+import { mobileWalletInstallMessage } from '../content'
+import { WalletModule, Helpers, CommonWalletOptions } from '../../../interfaces'
+
+import oneInchIcon from '../wallet-icons/1inch.png'
+import oneInchIcon2x from '../wallet-icons/1inch@2x.png'
+
+function oneInchWallet(
+  options: CommonWalletOptions
+): WalletModule {
+  const { preferred, label, iconSrc, svg } = options
+
+  return {
+    name: label || '1inch',
+    iconSrc: iconSrc || oneInchIcon,
+    iconSrcSet: iconSrc || oneInchIcon2x,
+    svg,
+    wallet: async (helpers: Helpers) => {
+      const { createModernProviderInterface, getProviderName } = helpers
+
+      const provider =
+          (window as any).ethereum ||
+          ((window as any).web3 && (window as any).web3.currentProvider)
+
+      return {
+        provider,
+        interface:
+            provider && getProviderName(provider) === '1inch'
+                ? createModernProviderInterface(provider)
+                : null
+      }
+    },
+    type: 'injected',
+    link: `https://wallet.1inch.io?url=${window.location.host}`,
+    installMessage: mobileWalletInstallMessage,
+    mobile: true,
+    preferred,
+    osExclusions: ['Android']
+  }
+}
+
+export default oneInchWallet
