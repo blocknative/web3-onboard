67aca5ac86f7be36b88e4f556a734997a318c002
Aaron Barnard <abarnard@protonmail.com>
2022-02-03 17:17:38 +1100

    Fix binance icon

--- packages/injected/src/icons/binance.ts
+++ packages/injected/src/icons/binance.ts
@@ -1,5 +1,5 @@
 export default `
-<svg width="100%" height="100%" viewBox="0 0 300 300" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+<svg height="100%" viewBox="0 0 300 300" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
   <rect width="300" height="300" fill="url(#pattern0)"/>
   <defs>
     <pattern id="pattern0" patternContentUnits="objectBoundingBox" width="1" height="1">
