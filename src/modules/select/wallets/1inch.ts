95ce4593f70baa1ddf54e368d7006e0a31011e82
Adam Carpenter <adamcarpenter86@gmail.com>
2022-02-28 10:19:38 -0700

    Slight reversion for backwards compatibility

--- src/modules/select/wallets/1inch.ts
+++ src/modules/select/wallets/1inch.ts
@@ -16,7 +16,8 @@ function oneInchWallet(options: CommonWalletOptions): WalletModule {
       const { createModernProviderInterface, getProviderName } = helpers
       const windowAsAny = window as any
       const provider =
-        windowAsAny.ethereum
+        windowAsAny.ethereum ||
+        (windowAsAny.web3 && windowAsAny.web3.currentProvider)
 
       return {
         provider,
