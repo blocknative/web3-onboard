cb758b0460201f78e143c79bb82a7280c0364d6a
Adam Carpenter <adamcarpenter86@gmail.com>
2022-04-12 09:21:08 -0600

    Prettier-ify validation

--- packages/magic/src/validation.ts
+++ packages/magic/src/validation.ts
@@ -3,7 +3,9 @@ import type { MagicInitOptions } from './types'
 
 const magicOptions = Joi.object({
   apiKey: Joi.string().required(),
-  userEmail: Joi.string().email({ tlds: { allow: false } }).allow(null)
+  userEmail: Joi.string()
+    .email({ tlds: { allow: false } })
+    .allow(null)
 })
 
 type ValidateReturn = Joi.ValidationResult | null
