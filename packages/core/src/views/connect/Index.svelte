430b545b1432f057d40a26bef058ff08087d02ae
Adam Carpenter <adamcarpenter86@gmail.com>
2022-04-20 10:01:57 -0600

    test

--- packages/core/src/views/connect/Index.svelte
+++ packages/core/src/views/connect/Index.svelte
@@ -354,6 +354,7 @@
                 wallet: selectedWallet && selectedWallet.label
               }
             })}
+            Working!!!
           </h4>
           <div on:click={close} class="button-container absolute">
             <CloseButton />
