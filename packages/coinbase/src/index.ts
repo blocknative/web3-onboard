c9e7cce433ebfe400c965b46848b584816af5350
Taylor Dawson <taylorjdawson@gmail.com>
2022-05-09 14:18:42 -0500

    [fix] Duplicate coinbase wallets (#987)
    * [fix] Duplicate coinbase wallets
    * Minor refactor

--- packages/coinbase/src/index.ts
+++ packages/coinbase/src/index.ts
@@ -1,54 +1,57 @@
 import { WalletInit } from '@web3-onboard/common'
 
-function coinbaseWallet(options?: { darkMode?: boolean }): WalletInit {
-  const { darkMode = false } = options || {}
+function coinbaseWallet({
+  darkMode = false
+}: {
+  darkMode?: boolean
+}): WalletInit {
+  return ({ device }) =>
+    device.type === 'mobile'
+      ? {
+          label: 'Coinbase',
+          getIcon: async () => (await import('./icon.js')).default,
+          getInterface: async ({ chains, appMetadata }) => {
+            const [chain] = chains
+            const { name, icon } = appMetadata || {}
 
-  return () => {
-    return {
-      label: 'Coinbase',
-      getIcon: async () => (await import('./icon.js')).default,
-      getInterface: async ({ chains, appMetadata }) => {
-        const [chain] = chains
-        const { name, icon } = appMetadata || {}
+            const { CoinbaseWalletSDK } = await import('@coinbase/wallet-sdk')
 
-        const { CoinbaseWalletSDK } = await import('@coinbase/wallet-sdk')
+            const base64 = window.btoa(icon || '')
+            const appLogoUrl = `data:image/svg+xml;base64,${base64}`
 
-        const base64 = window.btoa(icon || '')
-        const appLogoUrl = `data:image/svg+xml;base64,${base64}`
+            const instance = new CoinbaseWalletSDK({
+              appName: name || '',
+              appLogoUrl,
+              darkMode
+            })
 
-        const instance = new CoinbaseWalletSDK({
-          appName: name || '',
-          appLogoUrl,
-          darkMode
-        })
+            const coinbaseWalletProvider = instance.makeWeb3Provider(
+              chain.rpcUrl,
+              parseInt(chain.id)
+            )
 
-        const coinbaseWalletProvider = instance.makeWeb3Provider(
-          chain.rpcUrl,
-          parseInt(chain.id)
-        )
+            // patch the chainChanged event
+            const on = coinbaseWalletProvider.on.bind(coinbaseWalletProvider)
+            coinbaseWalletProvider.on = (event, listener) => {
+              on(event, val => {
+                if (event === 'chainChanged') {
+                  listener(`0x${(val as number).toString(16)}`)
+                  return
+                }
 
-        // patch the chainChanged event
-        const on = coinbaseWalletProvider.on.bind(coinbaseWalletProvider)
-        coinbaseWalletProvider.on = (event, listener) => {
-          on(event, val => {
-            if (event === 'chainChanged') {
-              listener(`0x${(val as number).toString(16)}`)
-              return
+                listener(val)
+              })
+
+              return coinbaseWalletProvider
             }
 
-            listener(val)
-          })
-
-          return coinbaseWalletProvider
+            return {
+              provider: coinbaseWalletProvider,
+              instance
+            }
+          }
         }
-
-        return {
-          provider: coinbaseWalletProvider,
-          instance
-        }
-      }
-    }
-  }
+      : null
 }
 
 export default coinbaseWallet
