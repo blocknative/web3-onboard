189def228ec81b41a55dc0b6b5bd939c49e80a28
Adam Carpenter <adamcarpenter86@gmail.com>
2022-05-24 11:16:36 -0600

    Update coinbase package to remove mobile check

--- packages/coinbase/src/index.ts
+++ packages/coinbase/src/index.ts
@@ -5,53 +5,52 @@ function coinbaseWallet({
 }: {
   darkMode?: boolean
 } = {}): WalletInit {
-  return ({ device }) =>
-    device.type === 'mobile'
-      ? {
-          label: 'Coinbase Wallet',
-          getIcon: async () => (await import('./icon.js')).default,
-          getInterface: async ({ chains, appMetadata }) => {
-            const [chain] = chains
-            const { name, icon } = appMetadata || {}
+  return () => {
+    return {
+      label: 'Coinbase Wallet',
+      getIcon: async () => (await import('./icon.js')).default,
+      getInterface: async ({ chains, appMetadata }) => {
+        const [chain] = chains
+        const { name, icon } = appMetadata || {}
 
-            const { CoinbaseWalletSDK } = await import('@coinbase/wallet-sdk')
+        const { CoinbaseWalletSDK } = await import('@coinbase/wallet-sdk')
 
-            const base64 = window.btoa(icon || '')
-            const appLogoUrl = `data:image/svg+xml;base64,${base64}`
+        const base64 = window.btoa(icon || '')
+        const appLogoUrl = `data:image/svg+xml;base64,${base64}`
 
-            const instance = new CoinbaseWalletSDK({
-              appName: name || '',
-              appLogoUrl,
-              darkMode
-            })
+        const instance = new CoinbaseWalletSDK({
+          appName: name || '',
+          appLogoUrl,
+          darkMode
+        })
 
-            const coinbaseWalletProvider = instance.makeWeb3Provider(
-              chain.rpcUrl,
-              parseInt(chain.id)
-            )
+        const coinbaseWalletProvider = instance.makeWeb3Provider(
+          chain.rpcUrl,
+          parseInt(chain.id)
+        )
 
-            // patch the chainChanged event
-            const on = coinbaseWalletProvider.on.bind(coinbaseWalletProvider)
-            coinbaseWalletProvider.on = (event, listener) => {
-              on(event, val => {
-                if (event === 'chainChanged') {
-                  listener(`0x${(val as number).toString(16)}`)
-                  return
-                }
-
-                listener(val)
-              })
-
-              return coinbaseWalletProvider
+        // patch the chainChanged event
+        const on = coinbaseWalletProvider.on.bind(coinbaseWalletProvider)
+        coinbaseWalletProvider.on = (event, listener) => {
+          on(event, val => {
+            if (event === 'chainChanged') {
+              listener(`0x${(val as number).toString(16)}`)
+              return
             }
 
-            return {
-              provider: coinbaseWalletProvider,
-              instance
-            }
-          }
+            listener(val)
+          })
+
+          return coinbaseWalletProvider
         }
-      : null
+
+        return {
+          provider: coinbaseWalletProvider,
+          instance
+        }
+      }
+    }
+  }
 }
 
 export default coinbaseWallet
