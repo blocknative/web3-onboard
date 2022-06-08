462dc213aa02fdd454c23fbd22a2fda81a7d0c4d
Aaron Barnard <abarnard@protonmail.com>
2022-06-08 13:37:04 +1000

    Remove network from customize notification

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -185,9 +185,11 @@ export type Notification = {
 
 export type TransactionHandlerReturn = CustomNotification | boolean | void
 
-export type CustomNotification = Partial<Omit<Notification, 'id' | 'startTime'>>
+export type CustomNotification = Partial<
+  Omit<Notification, 'id' | 'startTime' | 'network'>
+>
 
-export type NotificationType = 'pending' | 'success' | 'error' | 'hint'
+export type NotificationType = 'pending' | 'success' //| 'error' | 'hint'
 
 // ==== ACTIONS ==== //
 export type Action =
