55f02d34372105b643078337e23c93dd5f42b717
Mahmud Adeleye <mahmud@blocknative.com>
2022-07-05 17:25:17 -0400

    refactor

--- packages/demo/src/App.svelte
+++ packages/demo/src/App.svelte
@@ -62,9 +62,7 @@
 
   const coinbaseWallet = coinbaseModule()
 
-  const walletConnect = walletConnectModule({
-    connectFirstChainId: true
-  })
+  const walletConnect = walletConnectModule()
   const portis = portisModule({
     apiKey: 'b2b7586f-2b1e-4c30-a7fb-c2d1533b153b'
   })
