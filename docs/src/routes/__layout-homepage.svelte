f3b8b39fd6ab2d1fa7009e570d22091565bd2fcd
Gustavo Esquinca <g.esq.ca@gmail.com>
2022-08-15 19:03:59 -0400

    add head info for homepage layout

--- docs/src/routes/__layout-homepage.svelte
+++ docs/src/routes/__layout-homepage.svelte
@@ -1,2 +1,22 @@
 <!-- layout-homepage -->
+<script>
+  let title = 'My Title'
+  let description = 'My Description'
+</script>
+
+<svelte:head>
+  {#if title}
+    <title>{title}</title>
+  {/if}
+  {#if description}
+    <meta name="description" content={description} />
+  {/if}
+</svelte:head>
+
 <slot />
+
+<style>
+  :global(:root) {
+    /* root styles */
+  }
+</style>
