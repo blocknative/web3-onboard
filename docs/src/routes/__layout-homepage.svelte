dc5d9eae12b97cbd7f945be05c30fb8446aa93c8
Gustavo Esquinca <g.esq.ca@gmail.com>
2022-08-23 17:16:07 -0400

    move font-face styles
    - move @font-face declarations to fonts.css
    - update layout imports

--- docs/src/routes/__layout-homepage.svelte
+++ docs/src/routes/__layout-homepage.svelte
@@ -1,13 +1,13 @@
 <!-- layout-homepage -->
 <script>
-  import '../app.css'
-
   import '@svelteness/kit-docs/client/polyfills/index.js';
   import '@svelteness/kit-docs/client/styles/normalize.css';
   import '@svelteness/kit-docs/client/styles/theme.css';
   // import '@svelteness/kit-docs/client/styles/fonts.css';
   // import '@svelteness/kit-docs/client/styles/vars.css';
+  import '$lib/styles/fonts.css';
   import '$lib/styles/kit-docs.css';
+  import '../app.css'
 
   import {
     KitDocs,
--- docs/src/routes/__layout-kit-docs.svelte
+++ docs/src/routes/__layout-kit-docs.svelte
@@ -10,14 +10,14 @@
 </script>
 
 <script>
-  import '../app.css'
-
   import '@svelteness/kit-docs/client/polyfills/index.js';
   import '@svelteness/kit-docs/client/styles/normalize.css';
   import '@svelteness/kit-docs/client/styles/theme.css';
   // import '@svelteness/kit-docs/client/styles/fonts.css';
   // import '@svelteness/kit-docs/client/styles/vars.css';
+  import '$lib/styles/fonts.css';
   import '$lib/styles/kit-docs.css';
+  import '../app.css'
 
   import { page } from '$app/stores'
 
