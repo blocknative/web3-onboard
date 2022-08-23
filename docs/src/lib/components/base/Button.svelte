e069da71501bb23abd1f6d42fc399033eb1e50d7
Gustavo Esquinca <g.esq.ca@gmail.com>
2022-08-22 21:54:48 -0400

    update Button component
    - add link + icon styles
    - use anchor element
    - tweak button style

--- docs/src/lib/components/base/Button.svelte
+++ docs/src/lib/components/base/Button.svelte
@@ -1,44 +1,96 @@
 <script>
+  import ArrowRightLine from '~icons/ri/arrow-right-line';
+
   export let label = ''
+  export let href = '#!'
+  export let buttonStyle = 'button'
+
+  let iconSize = '24px';
 </script>
 
-<button type="button" style={$$props.style} on:click>
-  <slot>
-    {label}
-  </slot>
-</button>
+{#if buttonStyle === 'link'}
+  <a href={href} class="link-style" style={$$props.style}>
+    <slot>{label}</slot>
+    <ArrowRightLine width={iconSize} height={iconSize}/>
+  </a>
+{:else}
+  <a href={href} class="button-style" style={$$props.style}>
+    <slot>{label}</slot>
+  </a>
+{/if}
 
 <style>
-  button {
-    --text-color: #eff1fc;
-    --bg-color: #6370e5;
+  .button-style {
+    /* blue/400 */
+    --accent-color-rgb: 99, 112, 229;
+    /* vars */
+    --text-color: white;
+    --bg-color: rgba( var(--accent-color-rgb) , 1 );
 
-    /* brand/blue */
-    background: var(--bg-color);
+    /* color styles */
     color: var(--text-color);
+    background: var(--bg-color);
 
+    /* Auto layout */
     display: inline-flex;
     flex-direction: row;
     justify-content: center;
     align-items: center;
-    flex: var(--flex, auto);
+    gap: 8px;
 
     border: none;
     border-radius: 4px;
     padding: 1rem 2rem;
 
+    /* text */
     font-family: 'Sofia Pro';
     font-style: normal;
     font-weight: 700;
-    font-size: 16px;
-    line-height: 16px;
+    font-size: 1rem;
+    line-height: 1rem;
+    text-align: center;
+
+    transition: 300ms ease-out;
   }
 
-  button:hover {
-    /* --text-color: black; */
-    --bg-color: #454ea0;
+  .button-style:hover {
+    --text-color: black;
+    --bg-color: white;
   }
 
-  button:focus {
+  .link-style {
+    /* blue/400 */
+    --accent-color-rgb: 146, 155, 237;
+    /* vars */
+    --text-color: rgba( var(--accent-color-rgb) , 1 );
+    --bg-color: rgba( var(--accent-color-rgb) , 0 );
+
+    /* color styles */
+    color: var(--text-color);
+    background: var(--bg-color);
+
+    border-radius: 8px;
+
+    /* Auto layout */
+    display: inline-flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    gap: 8px;
+    padding: 0.75rem 1rem;
+
+    /* text */
+    font-family: 'Sofia Pro';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 1rem;
+    line-height: 1rem;
+    text-align: center;
+
+    transition: 300ms ease-out;
   }
+  .link-style:hover {
+    --bg-color: rgba( var(--accent-color-rgb) , 0.2 );
+  }
+
 </style>
