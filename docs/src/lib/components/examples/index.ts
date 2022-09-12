7325977f38633ae8e01c2ce3d54af7dcae514406
Adam Carpenter <adamcarpenter86@gmail.com>
2022-09-12 14:51:09 -0600

    start on theming customizer

--- docs/src/lib/components/examples/index.ts
+++ docs/src/lib/components/examples/index.ts
@@ -1,3 +1,4 @@
 export { default as ConnectWallet } from './ConnectWallet.svelte'
+export { default as ThemeCustomizer } from './ThemeCustomizer.svelte'
 export { default as ReactConnectWallet } from './ReactConnectWallet.md'
 export { default as SvelteConnectWallet } from './SvelteConnectWallet.md'
