e36b3746a415501a9eb9ca301a7eaee765d1acc6
Adam Carpenter <adamcarpenter86@gmail.com>
2022-09-16 12:04:15 -0600

    Remove from sidebar

--- docs/src/lib/icons/LinkOutIcon.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  let linkOut = `<svg width="100%" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M12 4L10.59 5.41L16.17 11H4V13H16.17L10.59 18.59L12 20L20 12L12 4Z" fill="currentColor"/>
-</svg>`
-</script>
-
-<div style="width: 24px; height: 24px;">
-  {@html linkOut}
-</div>
