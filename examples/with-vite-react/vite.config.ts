cab41b146e49480bef64cf23f3b9ed324c3c3dde
Taylor Dawson <taylorjdawson@gmail.com>
2022-10-21 15:18:11 -0700

    Feature/with vite react (#1329)
    * Add working vite example
    * Add exclude dep
    * minor updates

---
+++ examples/with-vite-react/vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+
+import react from '@vitejs/plugin-react'
+import nodePolyfills from 'vite-plugin-node-stdlib-browser'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), nodePolyfills()],
+  optimizeDeps: {
+    exclude: ['@ethersproject/hash', 'wrtc'],
+    include: ['js-sha3', '@ethersproject/bignumber']
+  }
+})
