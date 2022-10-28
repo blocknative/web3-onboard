d2ceccda8245fab484a7e8c64c7fbd80c113bf26
Gamaliel Padillo <gamalielgad1234@gmail.com>
2022-10-27 17:45:38 -0700

    devop: add check for ios unsupported chains

--- packages/mew-wallet/src/index.ts
+++ packages/mew-wallet/src/index.ts
@@ -1,12 +1,12 @@
 import type { WalletInit } from '@web3-onboard/common'
 import { createEIP1193Provider } from '@web3-onboard/common'
 import { CustomWindow } from './types.js'
-import platform from 'platform';
 declare const window: CustomWindow
 
 function mewWallet(): WalletInit {
   if (typeof window === 'undefined') return () => null
   return ({ device }) => {
+    console.log(device, 'AAAAAAA')
     return device.type === 'mobile'
       ? {
         label: 'MEW wallet',
@@ -18,12 +18,10 @@ function mewWallet(): WalletInit {
             return {
               provider: createEIP1193Provider(window.ethereum, {
                 wallet_switchEthereumChain: async ({ params }) => {
-                  const iosChains = ['0x1'];
-                  const androidChains = ['0x1', '0x89', '0x38'];
-                  const chains = platform.manufacturer.toLowerCase() === 'apple' ? iosChains : androidChains;
-                  const supported = chains.find(id => id === params[0].chainId)
-                  if (!supported) throw new Error("MEW Wallet doesn't support selected chain");
-                  window.ethereum.setChainId(parseInt(supported));
+                  if (device.os.name.toLowerCase() === 'ios') {
+                    throw new Error("MEW Wallet doesn't support selected chain");
+                  }
+                  window.ethereum.setChainId(parseInt(params[0].chainId));
                   return null;
                 }
               })
