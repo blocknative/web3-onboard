ae9eb3931c6fde74df42dac129f0105f9873355c
Vincent Shadbolt <vince.shadbolt@gmail.com>
2022-11-15 13:40:06 -0800

    updated uauth icon and added function return type

--- packages/uauth/src/index.ts
+++ packages/uauth/src/index.ts
@@ -30,7 +30,7 @@ const isHexString = (value: string | number) => {
   return true
 }
 
-export async function getUauthUser(options: uauthOptions) {
+export async function getUauthUser(options: uauthOptions): Promise<UserInfo> {
   const { clientID, redirectUri, scope = 'openid wallet' } = options || {}
 
   const uauthInstance = new UAuth({
@@ -58,8 +58,7 @@ function uauth(options: uauthOptions): WalletInit {
 
     return {
       label: 'Unstoppable',
-      getIcon: async () =>
-        '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M22.7319 2.06934V9.87229L0 19.094L22.7319 2.06934Z" fill="#2FE9FF"/><path fill-rule="evenodd" clip-rule="evenodd" d="M18.4696 1.71387V15.1917C18.4696 19.1094 15.2892 22.2853 11.3659 22.2853C7.44265 22.2853 4.26221 19.1094 4.26221 15.1917V9.51682L8.52443 7.17594V15.1917C8.52443 16.5629 9.63759 17.6745 11.0107 17.6745C12.3839 17.6745 13.497 16.5629 13.497 15.1917V4.4449L18.4696 1.71387Z" fill="#4C47F7"/></svg>',
+      getIcon: async () => (await import('./icon.js')).default,
       getInterface: async ({ chains, EventEmitter }) => {
         const uauthInstance = new UAuth({
           clientID: clientID,
