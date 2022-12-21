4d9d5e7e8d01a210dfa06b4e7ebcdea86cda1ab4
Gustavo Esquinca <g.esq.ca@gmail.com>
2022-12-21 12:37:16 -0500

    update blocknative-icon svg

--- packages/core/src/icons/blocknative-icon.ts
+++ packages/core/src/icons/blocknative-icon.ts
@@ -1,37 +1,26 @@
-export default `<svg width="72" height="72" viewBox="0 0 72 72" fill="none" xmlns="http://www.w3.org/2000/svg">
-<g filter="url(#filter0_d_15477_4703)">
-<rect x="8" y="6" width="56" height="56" rx="16" fill="#262A3D"/>
-<g clip-path="url(#clip0_15477_4703)">
-<path d="M28.6175 41.98L24.014 33.9948H33.2217L37.8259 41.98H28.6175Z" fill="#262A3D"/>
-<path d="M28.6175 41.98L24.014 33.9948H33.2217L37.8259 41.98H28.6175Z" fill="white"/>
-<path d="M33.2217 33.9948H24.014L28.6175 26.0096H37.8259L33.2217 33.9948Z" fill="url(#paint0_linear_15477_4703)"/>
-<path d="M42.4301 33.9948L37.8259 26.0097H28.6175L24.014 18.0245H42.4301L51.6378 33.9948H42.4301Z" fill="#262A3D"/>
-<path d="M42.4301 33.9948L37.8259 26.0097H28.6175L24.014 18.0245H42.4301L51.6378 33.9948H42.4301Z" fill="white"/>
-<path d="M42.4301 49.9652H24.014L28.6175 41.98H37.8259L42.4301 33.9948H51.6378L42.4301 49.9652Z" fill="url(#paint1_linear_15477_4703)"/>
-</g>
+export default `<svg width="56" height="56" viewBox="0 0 56 56" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect width="56" height="56" rx="8" fill="#262A3D"/>
+<g clip-path="url(#clip0_15747_103507)">
+<path d="M20.6176 35.9801L16.0142 27.9949H25.2218L29.826 35.9801H20.6176Z" fill="#262A3D"/>
+<path d="M20.6176 35.9801L16.0142 27.9949H25.2218L29.826 35.9801H20.6176Z" fill="white"/>
+<path d="M25.2218 27.9947H16.0142L20.6176 20.0095H29.826L25.2218 27.9947Z" fill="url(#paint0_linear_15747_103507)"/>
+<path d="M34.4302 27.9948L29.826 20.0096H20.6176L16.0142 12.0244H34.4302L43.6379 27.9948H34.4302Z" fill="#262A3D"/>
+<path d="M34.4302 27.9948L29.826 20.0096H20.6176L16.0142 12.0244H34.4302L43.6379 27.9948H34.4302Z" fill="white"/>
+<path d="M34.4302 43.9652H16.0142L20.6176 35.9801H29.826L34.4302 27.9949H43.6379L34.4302 43.9652Z" fill="url(#paint1_linear_15747_103507)"/>
 </g>
 <defs>
-<filter id="filter0_d_15477_4703" x="0" y="0" width="72" height="72" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-<feFlood flood-opacity="0" result="BackgroundImageFix"/>
-<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
-<feOffset dy="2"/>
-<feGaussianBlur stdDeviation="4"/>
-<feComposite in2="hardAlpha" operator="out"/>
-<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.2 0"/>
-<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_15477_4703"/>
-<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_15477_4703" result="shape"/>
-</filter>
-<linearGradient id="paint0_linear_15477_4703" x1="24.014" y1="30.0022" x2="37.8259" y2="30.0022" gradientUnits="userSpaceOnUse">
+<linearGradient id="paint0_linear_15747_103507" x1="16.0142" y1="24.0021" x2="29.826" y2="24.0021" gradientUnits="userSpaceOnUse">
 <stop stop-color="#55CCFE"/>
 <stop offset="1" stop-color="#5E93EF"/>
 </linearGradient>
-<linearGradient id="paint1_linear_15477_4703" x1="84.5101" y1="5220.05" x2="10399.8" y2="5220.05" gradientUnits="userSpaceOnUse">
+<linearGradient id="paint1_linear_15747_103507" x1="76.5102" y1="5214.05" x2="10391.8" y2="5214.05" gradientUnits="userSpaceOnUse">
 <stop stop-color="#55CCFE"/>
 <stop offset="1" stop-color="#5E93EF"/>
 </linearGradient>
-<clipPath id="clip0_15477_4703">
-<rect width="27.6667" height="32" fill="white" transform="translate(24 18)"/>
+<clipPath id="clip0_15747_103507">
+<rect width="27.6667" height="32" fill="white" transform="translate(16 12)"/>
 </clipPath>
 </defs>
 </svg>
+
 `
