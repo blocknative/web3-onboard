487f21e198923f797b3cc06baf36746bb7053570
Aaron <abarnard@protonmail.com>
2023-01-12 14:08:10 +1100

    Add examples of how to use new config options

--- packages/demo/src/App.svelte
+++ packages/demo/src/App.svelte
@@ -67,13 +67,39 @@
 
   const injected = injectedModule({
     custom: [
-      // include custom injected wallet modules here
-    ],
-    displayUnavailable: true,
-    filter: {
-      [ProviderLabel.Binance]: 'unavailable',
-      [ProviderLabel.Bitski]: 'unavailable'
-    }
+      // include custom (not natively supported) injected wallet modules here
+    ]
+    // display all wallets even if they are unavailable
+    // displayUnavailable: true
+    // but only show Binance and Bitski wallet if they are available
+    // filter: {
+    //   [ProviderLabel.Binance]: 'unavailable',
+    //   [ProviderLabel.Bitski]: 'unavailable'
+    // }
+    // do a manual sort of injected wallets so that MetaMask and Coinbase are ordered first
+    // sort: wallets => {
+    //   const metaMask = wallets.find(
+    //     ({ label }) => label === ProviderLabel.MetaMask
+    //   )
+    //   const coinbase = wallets.find(
+    //     ({ label }) => label === ProviderLabel.Coinbase
+    //   )
+
+    //   return (
+    //     [
+    //       metaMask,
+    //       coinbase,
+    //       ...wallets.filter(
+    //         ({ label }) =>
+    //           label !== ProviderLabel.MetaMask &&
+    //           label !== ProviderLabel.Coinbase
+    //       )
+    //     ]
+    //       // remove undefined values
+    //       .filter(wallet => wallet)
+    //   )
+    // }
+    // walletUnavailableMessage: wallet => `Oops ${wallet.label} is unavailable!`
   })
 
   const coinbaseWallet = coinbaseModule()
