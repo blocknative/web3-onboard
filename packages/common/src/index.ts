50fa6d7231846cdb48156e21ae54f480f5ad2b07
Adam Carpenter <adamcarpenter86@gmail.com>
2023-03-15 16:07:43 -0600

    Remove semi-bold

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,6 +1,6 @@
 export { ProviderRpcError } from './errors.js'
 export { createEIP1193Provider } from './eip-1193.js'
-export { SofiaProLight, SofiaProRegular, SofiaProSemiBold } from './fonts.js'
+export { SofiaProLight, SofiaProRegular } from './fonts.js'
 export { weiToEth } from './utils.js'
 
 export * from './types.js'
