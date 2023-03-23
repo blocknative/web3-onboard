e2a3fc9f223dff5caba52c33d6cd98738c09f139
Kat Leight <leightkt@gmail.com>
2023-03-23 14:25:49 -0600

    add sign message test

--- test/tests/metamask.spec.ts
+++ test/tests/metamask.spec.ts
@@ -1,14 +1,24 @@
 import { test, expect } from './fixtures'
 import * as metamask from '@synthetixio/synpress/commands/metamask'
 
+const connectMetamask = async page => {
+  await page.getByRole('button', { name: 'Connect Wallet' }).click()
+  await page.getByRole('checkbox').click()
+  await page.getByText('Metamask').click()
+  await metamask.acceptAccess()
+}
+
 test.beforeAll(async ({ page }) => {
   await page.goto('http://localhost:8080')
 })
 
 test('metamask connected', async ({ page }) => {
-  await page.getByRole('button', { name: 'Connect Wallet' }).click()
-  await page.getByRole('checkbox').click()
-  await page.getByText('Metamask').click()
-  await metamask.acceptAccess()
+  await connectMetamask(page)
   await expect(page.getByTestId('MetaMask')).toHaveText('MetaMask')
+})
+
+test('metamask sign message', async ({ page }) => {
+  await connectMetamask(page)
+  await page.getByRole('button', { name: 'Sign Message' }).click()
+  await metamask.confirmSignatureRequest()
 })
