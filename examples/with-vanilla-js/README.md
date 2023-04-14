5dbda4d00d680b6ae955b72fbe5ed6e9b85215a5
Kat Leight <leightkt@gmail.com>
2023-04-14 15:22:14 -0600

    styling and readme

---
+++ examples/with-vanilla-js/README.md
@@ -0,0 +1,33 @@
+This is a sample [Web3Onboard](https://github.com/blocknative/web3-onboard) project showing how to integrate web3Onboard with vanilla javascript.
+
+## Getting Started
+
+Clone this repo
+
+```
+https://github.com/blocknative/web3-onboard.git
+```
+
+Navigate to the project directory:
+
+```
+cd examples/with-vanilla-js
+```
+
+Install the dependencies:
+
+```
+npm install
+```
+
+Run the development server:
+
+```bash
+npm run dev
+```
+
+Use a local development server (like [Live Server Plugin](https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer) for vscode) to open the index.html page with your browser to see the result.
+
+## Learn More
+
+To learn more about other web3Onboard integrations, take a look at our documentation: [Web3Onboard Documentation](https://onboard.blocknative.com/docs/overview/introduction#features) for more details.
--- examples/with-vanilla-js/src/index.js
+++ examples/with-vanilla-js/src/index.js
@@ -1,5 +1,5 @@
 import onboard from './onboard'
-import "./styles.css"
+import './styles.css'
 
 const $connect = document.querySelector('.connect-button')
 const $disconnect = document.querySelector('.disconnect-button')
@@ -11,32 +11,35 @@ const $label = document.querySelector('.label')
 let label
 
 const connect = async () => {
-  wallets = await onboard.connectWallet()
+  return await onboard.connectWallet()
+}
+
+const disconnect = () => {
+  onboard.disconnectWallet({ label })
+}
+
+const addConnectedInfo = connectedAccount => {
+  const address = connectedAccount.address
+  const start = address.slice(0, 5)
+  const end = address.slice(-5, -1)
+  $address.innerHTML = `${start}.....${end}`
+  $label.innerHTML = `Connected Wallet: ${label}`
+}
+
+$connect.addEventListener('click', async _ => {
+  const wallets = await connect()
   const connectedAccount = wallets[0].accounts[0]
   label = wallets[0].label
   if (wallets[0]) {
+    console.log('in here')
     addConnectedInfo(connectedAccount)
     $wallet.classList.remove('hidden')
     $disconnected.classList.add('hidden')
   }
-}
+})
 
-const disconnect = () => {
+$disconnect.addEventListener('click', _ => {
+  disconnect()
   $wallet.classList.add('hidden')
   $disconnected.classList.remove('hidden')
-  onboard.disconnectWallet({ label })
-}
-
-const addConnectedInfo = (connectedAccount) => {
-  $address.innerHTML = connectedAccount.address
-  $label.innerHTML = `Connected Wallet: ${label}`
-}
-
-$connect.addEventListener('click', async (_) => {
-  await connect()
 })
-
-$disconnect.addEventListener('click', (_) => {
-  disconnect()
-})
-
