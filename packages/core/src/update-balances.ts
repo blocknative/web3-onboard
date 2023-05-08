515372922a5b600a1e0df7585c9ebb81b5eaf736
Adam Carpenter <adamcarpenter86@gmail.com>
2023-05-08 09:08:50 -0600

    Feat: Secondary Token Balances (#1679)
    * RPC working from hackathon
    
    * Working as expected for setting balances in state, no rendering yet
    
    * Display but need to be abstracted and cleaned
    
    * Added layout and styling for secondary tokens along with rework of maximized account center layout, still need docs updates for secondary tokens
    
    * Fix compiler err
    
    * Add docks for secondaryTokens and screenshots to readme and docs of AC
    
    * Fix versioning of common
    
    * Bump common
    
    * Bump all versions required
    
    * Prettify common validation

--- packages/core/src/update-balances.ts
+++ packages/core/src/update-balances.ts
@@ -1,6 +1,9 @@
 import { state } from './store/index.js'
 import { getBalance } from './provider.js'
 import { updateAllWallets } from './store/actions.js'
+import { ethers } from 'ethers'
+import { AccountAddress, Chain, weiToEth } from '@web3-onboard/common'
+import type { SecondaryTokenBalances, WalletState } from './types'
 
 async function updateBalances(addresses?: string[]): Promise<void> {
   const { wallets, chains } = state.get()
@@ -10,6 +13,11 @@ async function updateBalances(addresses?: string[]): Promise<void> {
 
       const updatedAccounts = await Promise.all(
         wallet.accounts.map(async account => {
+          const secondaryTokens = await updateSecondaryTokens(
+            wallet,
+            account.address,
+            chain
+          )
           // if no provided addresses, we want to update all balances
           // otherwise check if address is in addresses array
           if (
@@ -19,16 +27,53 @@ async function updateBalances(addresses?: string[]): Promise<void> {
             )
           ) {
             const updatedBalance = await getBalance(account.address, chain)
-            return { ...account, balance: updatedBalance }
+            return { ...account, balance: updatedBalance, secondaryTokens }
           }
-          return account
+          return { ...account, secondaryTokens }
         })
       )
       return { ...wallet, accounts: updatedAccounts }
     })
   )
-
   updateAllWallets(updatedWallets)
+}
+
+export const updateSecondaryTokens = async (
+  wallet: WalletState,
+  account: AccountAddress,
+  chain: Chain
+): Promise<SecondaryTokenBalances[]> => {
+  const chainRPC = chain.rpcUrl
+  if (!chain.secondaryTokens || !chain.secondaryTokens.length || !chainRPC)
+    return
+  const ethersProvider = new ethers.providers.Web3Provider(
+    wallet.provider,
+    'any'
+  )
+  const signer = ethersProvider.getSigner()
+  const tokenInterface = [
+    'function balanceOf(address owner) view returns (uint256)'
+  ]
+  const updatedBalances = await Promise.all(
+    chain.secondaryTokens.map(async token => {
+      try {
+        const swapContract = new ethers.Contract(
+          token.address,
+          tokenInterface,
+          signer
+        )
+        const bigNumBalance = await swapContract.balanceOf(account)
+        return {
+          name: token.name,
+          balance: weiToEth(bigNumBalance.toHexString()),
+          icon: token.icon
+        }
+      } catch (error) {
+        console.error(error)
+      }
+    })
+  )
+  return updatedBalances
 }
 
 export default updateBalances
