2158c5790461923c3b95ccbcc427b6b1ad22a878
Kat Leight <leightkt@gmail.com>
2023-05-09 13:52:03 -0600

    i18n type update

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -196,7 +196,11 @@ export type Configuration = {
 
 export type Locale = string
 export type i18nOptions = Record<Locale, i18n>
-export type i18n = typeof en
+// allows partial i18n override in TypeScript:
+type RecursivePartial<T> = { //https://stackoverflow.com/a/47914631
+  [P in keyof T]?: RecursivePartial<T[P]>;
+};
+export type i18n = RecursivePartial<typeof en>;
 
 export type ConnectModalOptions = {
   /**
