be87abb4c17e9879b3142a1051d93cfc7aca9cec
Kat Leight <33187102+leightkt@users.noreply.github.com>
2023-05-12 13:08:54 -0600

    Update docs/svelte.config.js
    Co-authored-by: Adam Carpenter <adamcarpenter86@gmail.com>

--- docs/svelte.config.js
+++ docs/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-static'
 import preprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
