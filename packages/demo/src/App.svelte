fd470ce396c0280662a090198ba5c8c974260455
Adam Carpenter <adamcarpenter86@gmail.com>
2023-05-15 13:54:17 -0600

    Merge in develop and bump package versions in demo

--- packages/demo/src/App.svelte
+++ packages/demo/src/App.svelte
@@ -151,8 +151,7 @@
   const trezorOptions = {
     email: 'test@test.com',
     appUrl: 'https://www.blocknative.com',
-
-     consecutiveEmptyAccountThreshold: 10
+    consecutiveEmptyAccountThreshold: 10
     // containerElement: '#sample-container-el'
   }
   const trezor = trezorModule(trezorOptions)
