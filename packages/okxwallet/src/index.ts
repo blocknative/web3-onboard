205037fae274f689a357d6bff99b01a1d4c93711
qinghui.tan <qinghui.tan@okg.com>
2023-05-25 16:10:49 +0800

    feature: update

---
+++ packages/okxwallet/src/index.ts
@@ -0,0 +1,34 @@
+import type { WalletInit, EIP1193Provider } from '@web3-onboard/common'
+
+import { CustomWindow } from './types.js'
+declare const window: CustomWindow
+
+function trust(): WalletInit {
+  if (typeof window === 'undefined') return () => null
+
+  return () => {
+    return {
+      label: 'OKX Wallet',
+      getIcon: async () => (await import('./icon.js')).default,
+      getInterface: async () => {
+        let provider: EIP1193Provider
+
+        if (window.okxwallet) {
+          // directly use the window.okxwallet injection
+          provider = window.okxwallet
+        } else {
+          // OKX wallet extension is not installed
+          // send user to install page
+          window.open('https://okx.com/download', '_blank')
+          throw new Error('Please Install OKX wallet to use this wallet')
+        }
+
+        return {
+          provider
+        }
+      }
+    }
+  }
+}
+
+export default trust
