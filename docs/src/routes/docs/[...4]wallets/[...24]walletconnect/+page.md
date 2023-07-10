7cc952fb730a585b254233cb298f5635d4449c0c
Adam Carpenter <adamcarpenter86@gmail.com>
2023-07-10 10:55:15 -0600

    Fix: Add WC validation for props, add prop to handle MetaMask usage through WC, Bump Coinbase sdk version (#1822)
    * Add validation to WC with custom messages and warning if explore url or new dappUrl is not present
    
    * Fix build errors update vconsole in demo per dependabot
    
    * Add explore url to onboard init within docs
    
    * revert docs from testing
    
    * Update usage and fallbacks for WC url to remove gettingStartedGuide url
    
    * Update coinbase wallet sdk version
    
    * Yarn docs
    
    * Add WC init object check
    
    * Remove testing dotenv

--- docs/src/routes/docs/[...4]wallets/[...24]walletconnect/+page.md
+++ docs/src/routes/docs/[...4]wallets/[...24]walletconnect/+page.md
@@ -47,7 +47,7 @@ type WalletConnectOptions = {
        */
       version: 1
       /**
-       * Custom URL Bridge must be defined for V1 usage. 
+       * Custom URL Bridge must be defined for V1 usage.
        * WalletConnect no longer supports a v1 bridge.
        * Upgrading to use WalletConnect v2 is recommended.
        * A potential bridge can be found here: 'https://derelay.rabby.io'
@@ -63,6 +63,12 @@ type WalletConnectOptions = {
        * Project ID associated with [WalletConnect account](https://cloud.walletconnect.com)
        */
       projectId: string
+      /**
+       * Defaults to `appMetadata.explore` that is supplied to the web3-onboard init
+       * Strongly recommended to provide atleast one URL as it is required by some wallets (i.e. MetaMask)
+       * To connect with walletconnect
+       */
+      dappUrl?: string
       /**
        * Defaults to version: 2
        */
@@ -121,15 +127,16 @@ const onboard = Onboard({
     walletConnect
     //... other wallets
   ],
-  chains: [ // chains that are passed as optional chains to WC wallet after cleaning and parsing as number[]
-      {
-        id: '0x89',
-        token: 'MATIC',
-        label: 'Polygon',
-        rpcUrl: 'https://matic-mainnet.chainstacklabs.com'
-      }
-      // ...
-    ]
+  chains: [
+    // chains that are passed as optional chains to WC wallet after cleaning and parsing as number[]
+    {
+      id: '0x89',
+      token: 'MATIC',
+      label: 'Polygon',
+      rpcUrl: 'https://matic-mainnet.chainstacklabs.com'
+    }
+    // ...
+  ]
 })
 
 const connectedWallets = await onboard.connectWallet()
--- packages/walletconnect/src/v2.ts
+++ packages/walletconnect/src/v2.ts
@@ -34,7 +34,8 @@ function walletConnect(options: WalletConnectOptions): WalletInit {
     requiredChains,
     optionalChains,
     qrModalOptions,
-    additionalOptionalMethods
+    additionalOptionalMethods,
+    dappUrl
   } = options
 
   return () => {
@@ -55,10 +56,20 @@ function walletConnect(options: WalletConnectOptions): WalletInit {
 
         const getMetaData = (): CoreTypes.Metadata | undefined => {
           if (!appMetadata) return undefined
+          const url =
+            dappUrl ||
+            appMetadata.explore ||
+            ''
+
+          !url &&
+            !url.length &&
+            console.warn(
+              `It is strongly recommended to supply a dappUrl as it is required by some wallets (i.e. MetaMask) to allow connection.`
+            )
           const wcMetaData: CoreTypes.Metadata = {
             name: appMetadata.name,
             description: appMetadata.description || '',
-            url: appMetadata.explore || appMetadata.gettingStartedGuide || '',
+            url,
             icons: []
           }
 
@@ -121,8 +132,11 @@ function walletConnect(options: WalletConnectOptions): WalletInit {
           public connector: InstanceType<typeof EthereumProvider>
           public chains: Chain[]
           public disconnect: EIP1193Provider['disconnect']
+          // @ts-ignore
           public emit: typeof EventEmitter['emit']
+          // @ts-ignore
           public on: typeof EventEmitter['on']
+          // @ts-ignore
           public removeListener: typeof EventEmitter['removeListener']
 
           private disconnected$: InstanceType<typeof Subject>
