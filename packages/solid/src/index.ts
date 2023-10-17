efe2926c84ca1d49b73bd87f8128da2d7800cb2e
Adam Carpenter <adamcarpenter86@gmail.com>
2023-10-17 09:34:29 -0600

    Fix failing typecheck caused by missing command in solidjs package

--- packages/solid/src/index.ts
+++ packages/solid/src/index.ts
@@ -1,6 +1,6 @@
-import { createSignal, createEffect, Ref, createMemo, Accessor, Setter, Signal } from "solid-js";
-import { SetStoreFunction, Store, createStore } from "solid-js/store";
-import Web3Onboard from "@web3-onboard/core";
+import { createSignal, createEffect, createMemo, Signal } from 'solid-js'
+import { SetStoreFunction, Store, createStore } from 'solid-js/store'
+import Web3Onboard from '@web3-onboard/core'
 import type {
   InitOptions,
   OnboardAPI,
@@ -8,12 +8,11 @@ import type {
   DisconnectOptions,
   WalletState,
   ConnectedChain,
-  AppState,
-} from "@web3-onboard/core";
-import {OnboardComposable,SetChainOptions} from "./types";
-export type * from "@web3-onboard/core"
-export type * from "./types"
-
+  AppState
+} from '@web3-onboard/core'
+import { OnboardComposable, SetChainOptions } from './types'
+export type * from '@web3-onboard/core'
+export type * from './types'
 
 export const STORAGE_KEYS = {
   TERMS_AGREEMENT: 'onboard.js:agreement',
@@ -22,19 +21,17 @@ export const STORAGE_KEYS = {
 
 function createLocalStore<T extends object>(
   name: string,
-  init: T,
+  init: T
 ): [Store<T>, SetStoreFunction<T>] {
-
-  const localState = localStorage.getItem(name);
+  const localState = localStorage.getItem(name)
 
   const [state, setState] = createStore<T>(
-    localState ? (JSON.parse(localState) as T) : init,
-  );
+    localState ? (JSON.parse(localState) as T) : init
+  )
   createEffect(() => {
-
-    localStorage.setItem(name, JSON.stringify(state));
-  });
-  return [state, setState];
+    localStorage.setItem(name, JSON.stringify(state))
+  })
+  return [state, setState]
 }
 
 function createLocalStorageSignal<T>(
@@ -42,29 +39,34 @@ function createLocalStorageSignal<T>(
   defaultValue: T,
   storage = localStorage
 ): Signal<T> {
+  const initialValue: T =
+    JSON.parse(storage.getItem(key) ?? '{}').value ?? defaultValue
 
-  const initialValue: T = JSON.parse(storage.getItem(key) ?? "{}").value ?? defaultValue;
-
-  const [value, setValue] = createSignal<T>(initialValue as T);
+  const [value, setValue] = createSignal<T>(initialValue as T)
 
   const setValueAndStore = ((arg: any) => {
-    const v = setValue(arg);
-    storage.setItem(key, JSON.stringify({ value: v }));
-    return v;
-  }) as typeof setValue;
+    const v = setValue(arg)
+    storage.setItem(key, JSON.stringify({ value: v }))
+    return v
+  }) as typeof setValue
 
-  return [value, setValueAndStore];
+  return [value, setValueAndStore]
 }
 
 // Onboard will be kept here to be reused every time that we access the composable
 let web3Onboard: OnboardAPI | null = null
 
-const [alreadyConnectedWallets, setAlreadyConnectedWallets] = createLocalStore<string[]>(STORAGE_KEYS.LAST_CONNECTED_WALLET, []);
-const [lastConnectionTimestamp, setLastConnectionTimestamp] = createLocalStorageSignal('lastConnectionTimestamp', 0)
-const [onboardState, setOnboardState] = createStore<AppState>({} as AppState);
+const [alreadyConnectedWallets, setAlreadyConnectedWallets] = createLocalStore<
+  string[]
+>(STORAGE_KEYS.LAST_CONNECTED_WALLET, [])
+const [lastConnectionTimestamp, setLastConnectionTimestamp] =
+  createLocalStorageSignal('lastConnectionTimestamp', 0)
+const [onboardState, setOnboardState] = createStore<AppState>({} as AppState)
 
 const updateAlreadyConnectedWallets = () => {
-  setAlreadyConnectedWallets(onboardState.wallets.map((w: WalletState) => w.label))
+  setAlreadyConnectedWallets(
+    onboardState.wallets.map((w: WalletState) => w.label)
+  )
 }
 
 const init = (options: InitOptions): OnboardAPI => {
@@ -75,8 +77,7 @@ const init = (options: InitOptions): OnboardAPI => {
     web3Onboard?.state.select().subscribe((update: AppState) => {
       setOnboardState(update)
       updateAlreadyConnectedWallets()
-    }
-    )
+    })
   })
 
   return web3Onboard
@@ -89,7 +90,7 @@ const useOnboard = (): OnboardComposable => {
   }
 
   // Wallet related functions and variables
-  const [connectingWallet, setConnectingWallet] = createSignal<boolean>(false);
+  const [connectingWallet, setConnectingWallet] = createSignal<boolean>(false)
   const wallets = createMemo(() => onboardState.wallets)
 
   const connectedWallet = createMemo<WalletState | null>(() =>
@@ -119,10 +120,7 @@ const useOnboard = (): OnboardComposable => {
   // Chain related functions and variables
   const [settingChain, setSettingChain] = createSignal<boolean>(false)
   const connectedChain = createMemo<ConnectedChain | null>(
-    () =>
-      (connectedWallet() &&
-        connectedWallet()!.chains[0]) ||
-      null
+    () => (connectedWallet() && connectedWallet()!.chains[0]) || null
   )
 
   const getChain = (walletLabel: string) => {
