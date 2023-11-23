aef72bbba0a2ef08e9da3764efebf0c0227ea5cb
abretonc7s <arthur.breton@consensys.net>
2023-11-23 20:20:38 +0800

    feat: metamask sdk v0.13.0

--- packages/metamask/src/index.ts
+++ packages/metamask/src/index.ts
@@ -1,18 +1,61 @@
 import type { MetaMaskSDKOptions } from '@metamask/sdk'
 import type { WalletInit } from '@web3-onboard/common'
 export type { MetaMaskSDKOptions } from '@metamask/sdk'
+import { MetaMaskSDK } from '@metamask/sdk'
+import type { createEIP1193Provider } from '@web3-onboard/common'
+
+type ImportSDK = {
+  createEIP1193Provider: typeof createEIP1193Provider
+  MetaMaskSDKConstructor: typeof MetaMaskSDK
+}
+
+const loadImports = async () => {
+  if (importPromise) {
+    return await importPromise
+  }
+
+  const { createEIP1193Provider } = await import('@web3-onboard/common')
+  const importedSDK = await import('@metamask/sdk')
+  const MetaMaskSDKConstructor =
+    // @ts-ignore
+    importedSDK.MetaMaskSDK || importedSDK.default.MetaMaskSDK
+
+  if (!MetaMaskSDKConstructor) {
+    throw new Error('Error importing and initializing MetaMask SDK')
+  }
+
+  return { createEIP1193Provider, MetaMaskSDKConstructor }
+}
+
+let importPromise: Promise<ImportSDK> | null = null
 
 function metamask({
   options
 }: {
   options: Partial<MetaMaskSDKOptions>
 }): WalletInit {
+  let sdk: MetaMaskSDK | null = null
+  let createInstance: typeof createEIP1193Provider
 
   return () => {
+    importPromise = loadImports().catch(error => {
+      throw error
+    })
+
+    const getProvider = (_sdk: MetaMaskSDK) => {
+      const provider = createInstance(_sdk.getProvider(), {})
+      provider.disconnect = () => {
+        sdk?.terminate()
+      }
+      return provider
+    }
+
     return {
       label: 'MetaMask',
       getIcon: async () => (await import('./icon.js')).default,
       getInterface: async ({ appMetadata }) => {
+        sdk = (window as any).mmsdk || sdk; // Prevent conflict with existing mmsdk instances
+
         if (sdk) {
           // Prevent re-initializing instance as it causes issues with MetaMask sdk mobile provider.
           return {
@@ -29,19 +72,17 @@ function metamask({
         // Patch issue with MetaMask SDK, remove after SDK is fixed
         localStorage.removeItem('providerType')
 
-        let MetaMaskSDKConstructor
-        if (!MetaMaskSDK) {
-          // @ts-ignore
-          MetaMaskSDKConstructor = metaMask.MetaMaskSDK
-        } else {
-          MetaMaskSDKConstructor = MetaMaskSDK
-        }
-
-        if (!MetaMaskSDKConstructor) {
+        if (
+          !imports?.MetaMaskSDKConstructor ||
+          !imports?.createEIP1193Provider
+        ) {
           throw new Error('Error importing and initializing MetaMask SDK')
         }
 
-        const sdk = new MetaMaskSDKConstructor({
+        const { createEIP1193Provider, MetaMaskSDKConstructor } = imports
+
+        createInstance = createEIP1193Provider
+        sdk = new MetaMaskSDKConstructor({
           ...options,
           dappMetadata: {
             name: options.dappMetadata?.name || name || '',
