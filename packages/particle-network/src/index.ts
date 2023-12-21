061d613c36c3a14f31b57d20264074c3a6c1ba63
TABASCO <84655644+TABASCOatw@users.noreply.github.com>
2023-12-21 05:21:34 -0700

    Fixes

--- packages/particle-network/src/index.ts
+++ packages/particle-network/src/index.ts
@@ -1,56 +1,84 @@
 import type { WalletInit } from '@web3-onboard/common';
-import { ParticleNetwork } from '@particle-network/auth';
+import { ParticleNetwork, Config } from '@particle-network/auth';
 import { ParticleProvider } from '@particle-network/provider';
 
-type AuthTypes = 'email' | 'phone' | 'google' | 'apple' | 'twitter' | 'facebook' | 'microsoft' | 'linkedin' | 'github' | 'twitch' | 'discord';
+type AuthTypes =
+	| 'email'
+	| 'phone'
+	| 'google'
+	| 'apple'
+	| 'twitter'
+	| 'facebook'
+	| 'microsoft'
+	| 'linkedin'
+	| 'github'
+	| 'twitch'
+	| 'discord';
 
 interface PreferredAuthType {
-  type: AuthTypes;
-  setAsDisplay: boolean;
+	type: AuthTypes;
+	setAsDisplay: boolean;
 }
 
-type ParticleAuthOptions = Omit<ParticleAuthModuleOptions, 'preferredAuthType'>;
-
-interface ParticleAuthModuleOptions extends ParticleAuthOptions {
-  preferredAuthType?: AuthTypes | PreferredAuthType;
+interface ParticleAuthModuleOptions extends Config {
+	preferredAuthType?: AuthTypes | PreferredAuthType;
 }
 
 const getDisplayLabel = (authType?: string, shouldSetDisplay?: boolean) => {
-  return shouldSetDisplay ? authType?.charAt(0).toUpperCase() + authType?.slice(1) : 'Particle Network';
+	if (authType) {
+		return shouldSetDisplay
+			? authType.charAt(0).toUpperCase() + authType.slice(1)
+			: 'Particle Network';
+	}
+	return 'Particle Network';
 };
 
 const particleAuth = (options: ParticleAuthModuleOptions): WalletInit => {
-  const { preferredAuthType, ...otherOptions } = options;
-  const isAuthTypeObject = typeof preferredAuthType === 'object';
-  const { type: authType, setAsDisplay } = isAuthTypeObject ? (preferredAuthType as PreferredAuthType) : {};
+	const { preferredAuthType, ...otherOptions } = options;
+	const isAuthTypeObject = typeof preferredAuthType === 'object';
+	const authType =
+		isAuthTypeObject && preferredAuthType ? preferredAuthType.type : undefined;
+	const setAsDisplay =
+		isAuthTypeObject && preferredAuthType
+			? preferredAuthType.setAsDisplay
+			: false;
 
-  const displayLabel = getDisplayLabel(authType, isAuthTypeObject && setAsDisplay);
+	const displayLabel = getDisplayLabel(authType, setAsDisplay);
 
-  return () => ({
-    label: displayLabel,
-    getIcon: async () => {
-      const iconName = (authType && (isAuthTypeObject ? setAsDisplay : false)) ? authType : 'icon';
-      return (await import(`./${iconName}.ts`)).default;
-    },
-    getInterface: async ({ EventEmitter, chains }) => {
-      const [currentChain] = chains;
-      const { label, id } = currentChain;
-      const chainProps = {
-        chainName: label.split(' ')[0].toLowerCase(),
-        chainId: parseInt(id.toString(), 16),
-      };
+	return () => ({
+		label: displayLabel,
+		getIcon: async () => {
+			const iconName = authType && setAsDisplay ? authType : 'icon';
+			return (await import(`./${iconName}.js`)).default;
+		},
+		getInterface: async ({ EventEmitter, chains }) => {
+			const [currentChain] = chains;
+			const { label, id } = currentChain;
 
-      const particle = new ParticleNetwork({ ...otherOptions, ...chainProps });
+			const chainName = label
+				? label.split(' ')[0].toLowerCase()
+				: 'defaultChainName';
+			const chainId = parseInt(id.toString(), 16);
 
-      const loginOptions = authType ? { preferredAuthType: authType } : undefined;
-      await particle.auth.login(loginOptions);
+			const particleConfig: Config = {
+				...otherOptions,
+				chainName,
+				chainId,
+			};
 
-      return {
-        provider: new ParticleProvider(particle.auth),
-        instance: particle,
-      };
-    },
-  });
+			const particle = new ParticleNetwork(particleConfig);
+
+			const loginOptions = authType
+				? { preferredAuthType: authType }
+				: undefined;
+			await particle.auth.login(loginOptions);
+
+			return {
+				provider: new ParticleProvider(particle.auth),
+				instance: particle,
+			};
+		},
+	});
 };
 
 export default particleAuth;
