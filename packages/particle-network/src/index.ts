c2febc3175248eba2e124a6ba2ab4232c632cb6f
TABASCO <84655644+TABASCOatw@users.noreply.github.com>
2024-01-03 18:42:09 -0700

    Update index.ts

--- packages/particle-network/src/index.ts
+++ packages/particle-network/src/index.ts
@@ -1,110 +1,114 @@
 import {
-	WalletInit,
-	EIP1193Provider,
-	ProviderRpcError,
-	ProviderRpcErrorCode,
-	ProviderAccounts,
-} from '@web3-onboard/common';
-import { createEIP1193Provider } from '@web3-onboard/common';
-import { ParticleNetwork, Config } from '@particle-network/auth';
-import { ParticleProvider } from '@particle-network/provider';
+  WalletInit,
+  EIP1193Provider,
+  ProviderRpcError,
+  ProviderRpcErrorCode,
+  ProviderAccounts
+} from '@web3-onboard/common'
+import { Config } from '@particle-network/auth'
 
 type AuthTypes =
-	| 'email'
-	| 'phone'
-	| 'google'
-	| 'apple'
-	| 'twitter'
-	| 'facebook'
-	| 'microsoft'
-	| 'linkedin'
-	| 'github'
-	| 'twitch'
-	| 'discord';
+  | 'email'
+  | 'phone'
+  | 'google'
+  | 'apple'
+  | 'twitter'
+  | 'facebook'
+  | 'microsoft'
+  | 'linkedin'
+  | 'github'
+  | 'twitch'
+  | 'discord'
 
 interface PreferredAuthType {
-	type: AuthTypes;
-	setAsDisplay: boolean;
+  type: AuthTypes
+  setAsDisplay: boolean
 }
 
 interface ParticleAuthModuleOptions extends Config {
-	preferredAuthType?: AuthTypes | PreferredAuthType;
+  preferredAuthType?: AuthTypes | PreferredAuthType
 }
 
 const getDisplayLabel = (authType?: string, shouldSetDisplay?: boolean) => {
-	if (authType) {
-		return shouldSetDisplay
-			? authType.charAt(0).toUpperCase() + authType.slice(1)
-			: 'Particle Network';
-	}
-	return 'Particle Network';
-};
+  if (authType) {
+    return shouldSetDisplay
+      ? authType.charAt(0).toUpperCase() + authType.slice(1)
+      : 'Particle Network'
+  }
+  return 'Particle Network'
+}
 
-const particleAuth = (options: ParticleAuthModuleOptions): WalletInit => {
-	const { preferredAuthType, ...otherOptions } = options;
-	const isAuthTypeObject = typeof preferredAuthType === 'object';
-	const authType =
-		isAuthTypeObject && preferredAuthType ? preferredAuthType.type : undefined;
-	const setAsDisplay =
-		isAuthTypeObject && preferredAuthType
-			? preferredAuthType.setAsDisplay
-			: false;
+const particleAuth = async (
+  options: ParticleAuthModuleOptions
+): Promise<WalletInit> => {
+  const { createEIP1193Provider } = await import('@web3-onboard/common')
+  const { ParticleNetwork } = await import('@particle-network/auth')
+  const { ParticleProvider } = await import('@particle-network/provider')
 
-	const displayLabel = getDisplayLabel(authType, setAsDisplay);
+  const { preferredAuthType, ...otherOptions } = options
+  const isAuthTypeObject = typeof preferredAuthType === 'object'
+  const authType =
+    isAuthTypeObject && preferredAuthType ? preferredAuthType.type : undefined
+  const setAsDisplay =
+    isAuthTypeObject && preferredAuthType
+      ? preferredAuthType.setAsDisplay
+      : false
 
-	return () => ({
-		label: displayLabel,
-		getIcon: async () => {
-			const iconName = authType && setAsDisplay ? authType : 'icon';
-			return (await import(`./${iconName}.svg`)).default;
-		},
-		getInterface: async ({ chains }) => {
-			let [currentChain] = chains;
-			const { label, id } = currentChain;
+  const displayLabel = getDisplayLabel(authType, setAsDisplay)
 
-			const chainName = label
-				? label.split(' ')[0].toLowerCase()
-				: 'defaultChainName';
-			const chainId = parseInt(id.toString(), 16);
+  return () => ({
+    label: displayLabel,
+    getIcon: async () => {
+      const iconName = authType && setAsDisplay ? authType : 'icon'
+      return (await import(`./${iconName}.svg`)).default
+    },
+    getInterface: async ({ chains }) => {
+      let [currentChain] = chains
+      const { label, id } = currentChain
 
-			const particleConfig: Config = {
-				...otherOptions,
-				chainName,
-				chainId,
-			};
+      const chainName = label
+        ? label.split(' ')[0].toLowerCase()
+        : 'defaultChainName'
+      const chainId = parseInt(id.toString(), 16)
 
-			let particle = new ParticleNetwork(particleConfig);
-			let provider = new ParticleProvider(particle.auth);
+      const particleConfig: Config = {
+        ...otherOptions,
+        chainName,
+        chainId
+      }
 
-			function patchProvider(): EIP1193Provider {
-				const patchedProvider = createEIP1193Provider(provider, {
-					eth_selectAccounts: null,
-					eth_requestAccounts: async ({ baseRequest }) => {
-						try {
-							const accounts = await baseRequest({ method: 'eth_accounts' });
-							return accounts as ProviderAccounts;
-						} catch (error) {
-							console.error(error);
-							throw new ProviderRpcError({
-								code: ProviderRpcErrorCode.ACCOUNT_ACCESS_REJECTED,
-								message: 'Account access rejected',
-							});
-						}
-					},
-				});
+      let particle = new ParticleNetwork(particleConfig)
+      let provider = new ParticleProvider(particle.auth)
 
-				patchedProvider.disconnect = () => particle.auth.logout();
-				return patchedProvider;
-			}
+      function patchProvider(): EIP1193Provider {
+        const patchedProvider = createEIP1193Provider(provider, {
+          eth_selectAccounts: null,
+          eth_requestAccounts: async ({ baseRequest }) => {
+            try {
+              const accounts = await baseRequest({ method: 'eth_accounts' })
+              return accounts as ProviderAccounts
+            } catch (error) {
+              console.error(error)
+              throw new ProviderRpcError({
+                code: ProviderRpcErrorCode.ACCOUNT_ACCESS_REJECTED,
+                message: 'Account access rejected'
+              })
+            }
+          }
+        })
 
-			provider = patchProvider();
+        patchedProvider.disconnect = () => particle.auth.logout()
+        return patchedProvider
+      }
 
-			return {
-				provider,
-				instance: particle,
-			};
-		},
-	});
-};
+      provider = patchProvider()
 
-export default particleAuth;
+      return {
+        provider,
+        instance: particle
+      }
+    }
+  })
+}
+
+export default particleAuth
