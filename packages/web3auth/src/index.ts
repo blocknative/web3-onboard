71c2b3030d8be572f8b540274d046a2c554d2461
Benjamin Gobeil <bkgobeil@gmail.com>
2024-06-10 14:54:09 -0400

    chore: update web3auth deps and fix build

--- packages/web3auth/src/index.ts
+++ packages/web3auth/src/index.ts
@@ -48,7 +48,7 @@ function web3auth(options: Web3AuthModuleOptions): WalletInit {
           ticker: token,
           tickerName: label,
           chainId: id,
-          rpcTarget: rpcUrl,
+          rpcTarget: rpcUrl ?? "", // TODO: Ask maintainers if we should throw instead.
           chainNamespace:
             namespace === 'evm'
               ? CHAIN_NAMESPACES.EIP155
